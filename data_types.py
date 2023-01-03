3765a0ed8f1c750c1b669aeaf617ab5763431232
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2023-01-03 13:42:08 +0530

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,46 @@
+c=1+3j
+print(c.real)
+print(c.imag)
+bool(0)
+print(bool(0))
+print(bool(-1))
+print(bool(True))
+x=int(124.78)
+print(x)
+x=int("2")
+print(x)
+x=int(True)
+print(x)
+#x=int(2+7j)
+#print(x)
+z=complex(1,3)
+print(z)
+y=complex(False)
+print(y)
+x=complex(False,True)
+print(x)
+print(bool(3))
+print(bool("city"))
+print(bool(None))
+print(bool(""))
+print(bool(4))
+print(bool(0))
+city="Mangalore"
+x=city[0]
+print(x)
+print(city[7])
+x=city[1:5]#slice operator
+print(x)
+x=city[2:6:2]
+print(x)
+print(city[1:])
+print(city[:5])
+print(city[::])
+print(city[-1:-7:-1])
+print(city[::3])
+print(city[::-1])#code to reverse the string
+print(city[::])
+print(city.upper())#prints string in upper case
+city1=" my city is Mangalore"
+print(city1.split())#splits the string at the specified separator and returns list
+print(len(city1))
