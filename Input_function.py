ca12411b73ba06e3f524e6935265c7ca5916d0c8
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2023-01-03 13:42:39 +0530

    Add files via upload

---
+++ Input_function.py
@@ -0,0 +1,15 @@
+#age=input()
+#print(f"Age is {age}")
+
+#age=input("Enter your age:")is always a string by default
+#new_age=age+5
+#print(f"Your new age is {new_age}")
+#print(type(age)
+#age=int(input("Enter age:"))
+#new_age=age+5
+#print(f"New age is {new_age}")
+
+
+interest_rate=float(input("Enter interest:"))
+print(type(interest_rate))
+print(interest_rate)
