87cb8f1e8698c6eb5536353dbadf946ba6f3320e
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2023-01-05 20:10:02 +0530

    Add files via upload

---
+++ Assignment1solution_4.py
@@ -0,0 +1,8 @@
+a=32
+b=24.5
+c=False
+d="Mangalore"
+print(f"Data type of a is {type(a)}")
+print(f"Data type of b is {type(b)}")
+print(f"Data type of c is {type(c)}")
+print(f"Data type of d is {type(d)}")
