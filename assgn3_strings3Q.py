e2b212f22b9f76b651a0c7a675a43074ad348248
Reshma-Batara <121529317+Reshma-Batara@users.noreply.github.com>
2023-02-03 20:05:30 +0530

    Add files via upload

---
+++ assgn3_strings3Q.py
@@ -0,0 +1,19 @@
+#3.Convert decimal number into octal number using format specifier
+x=65
+print("%o"%(x))
+print()
+#4. print float number with two decimal places
+float_number=123.76543
+print(round(float_number,2))
+print()
+print()
+#5.Take three different strings from user and assign it to three different variables
+x=input("First string:")
+y=input("Second string:")
+z=input("Third string:")
+print("String1=",x)
+print("String2=",y)
+print("String3=",z)
+print()
+print()
+
